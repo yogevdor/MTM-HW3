d3c76d6d6dff41815dcf20eca696565eec17e5d7
Dor Yogev <doryogevv@gmail.com>
2026-01-01 11:39:41 +0200

    added dtor, remove and filter

--- SortedList.h
+++ SortedList.h
@@ -3,37 +3,49 @@
 #include <iostream>
 #include <stdexcept>
 
-namespace mtm {
-
+namespace mtm
+{
     template <typename T>
-    class SortedList {
-      struct Node{
-        T value;
-        Node* next;
-        Node(const T& value, Node* next = nullptr) : value(value), next(next) {}
-      };
-    int size;
-    Node* head;
+    class SortedList
+    {
+        struct Node
+        {
+            T value;
+            Node* next;
+
+            Node(const T& value, Node* next = nullptr) : value(value), next(next)
+            {
+            }
+        };
+
+        int size;
+        Node* head;
 
     public:
-        SortedList(const SortedList<T> &other) : head(nullptr), size(0) {
-            if(other.head==nullptr){
+        SortedList(const SortedList<T>& other) : head(nullptr), size(0)
+        {
+            if (other.head == nullptr)
+            {
                 return;
             }
-            try {
+            try
+            {
                 this->head = new Node(other.head->value);
                 this->size++;
                 Node* current = this->head;
-                Node* otherCurrent= other.head->next;
-                while(otherCurrent!=nullptr){
+                Node* otherCurrent = other.head->next;
+                while (otherCurrent != nullptr)
+                {
                     current->next = new Node(otherCurrent->value);
                     current = current->next;
                     otherCurrent = otherCurrent->next;
                     this->size++;
                 }
             }
-            catch(...){
-                while(this->head!=nullptr){
+            catch (...)
+            {
+                while (this->head != nullptr)
+                {
                     Node* temp = this->head;
                     this->head = this->head->next;
                     delete temp;
@@ -42,8 +54,10 @@ namespace mtm {
             }
         }
 
-        SortedList<T>& operator=(const SortedList<T> &other) {
-            if (this == &other){
+        SortedList<T>& operator=(const SortedList<T>& other)
+        {
+            if (this == &other)
+            {
                 return *this;
             }
             SortedList<T> temp(other);
@@ -54,95 +68,145 @@ namespace mtm {
             this->size = other.size;
             return *this;
         }
+
+        SortedList();
+
+        ~SortedList()
+        {
+            while (this->head != nullptr)
+            {
+                Node* temp = this->head;
+                this->head = this->head->next;
+                delete temp;
+            }
+        }
+
+        void remove(ConstIterator it)
+        {
+            if (it.node == nullptr || head == nullptr) { return; }
+            Node* current = head;
+            if (it.node == head)
+            {
+                head = head->next;
+                delete current;
+                size--;
+                return;
+            }
+            while (current->next != nullptr && it.node != current->next)
+            {
+                current = current->next;
+            }
+            if (it.node == current->next)
+            {
+                Node* temp = current->next;
+                current->next = current->next->next;
+                delete temp;
+                size--;
+            }
+        }
+
         /**
-         *
-         * the class should support the following public interface:
-         * if needed, use =defualt / =delete
-         *
-         * constructors and destructor:
-     */
-         SortedList();
-     /**
-         * 2. copy constructor
-         * 3. operator= - assignment operator
-     */
-         void SortedList<T>::insert(const T& value){
-         if(head == nullptr || head->value  > value){
-            head = new Node(value, head);
+        */
+        void mtm::SortedList<T>::insert(const T& value)
+        {
+            if (head == nullptr || head->value > value)
+            {
+                head = new Node(value, head);
+                size++;
+                return;
+            }
+            Node* current = head;
+            while ((current->next->value) < value && current->next != nullptr)
+            {
+                current = current->next;
+            }
+            current->next = new Node(value, current->next);
             size++;
-            return;
-         }
-          Node* current = head;
-          while((current-> next-> value) < value && current->next != nullptr){
-           current = current->next;
-          }
-          current->next = new Node(value, current->next);
-          size++;
-         }
-          
+        }
+
+        template <class Predicate>
+        SortedList filter(Predicate func)
+        {
+            SortedList filtered;
+            Node* current = this->head;
+            while (current != nullptr)
+            {
+                if (func(current->value))
+                {
+                    filtered.insert(func(current->value));
+                }
+                current = current->next;
+            }
+            return filtered;
+        }
+
         /**
-         * 4. ~SortedList() - destructor
          *
          * iterator:
          * 5. class ConstIterator;
          * 6. begin method
      */
-     const T& operator*() const {
-          Node* current = head;
-          while(current->next != nullptr){
-           current = current->next;
-          }
-          return current;
-         }
-     /**
-         * 7. end method
-         *
-         * functions:
-         * 8. insert - inserts a new element to the list
-         * 9. remove - removes an element from the list
-     */
-     int SortedList<T>::length(const T& value){
-         return size;
-     }
+        const T& operator*() const
+        {
+            Node* current = head;
+            while (current->next != nullptr)
+            {
+                current = current->next;
+            }
+            return current;
+        }
 
-      /**   * 10. length - returns the number of elements in the list
-         * 11. filter - returns a new list with elements that satisfy a given condition
-     */
-     SortedList<T> SortedList<T>::apply(T (*operation)(const T&)) const{
-          SortedList<T> newList;
-          Node* current = head;
-          ;
-          while(current != nullptr){
-           T temp = operation(current->value);
-           newList.insert(temp);
-           current = current->next;
-          }
-          return newList;
-     }
-     /**
-         * 12. apply - returns a new list with elements that were modified by an operation
-         */
+        /**
+            * 7. end method
+            *
+            * functions:
+            * 8. insert - inserts a new element to the list
+        */
+        int mtm::SortedList<T>::length(const T& value)
+        {
+            return size;
+        }
 
+        /**   * 10. length - returns the number of elements in the list
+           * 11. filter - returns a new list with elements that satisfy a given condition
+       */
+        SortedList<T> mtm::SortedList<T>::apply(T (*operation)(const T&)) const
+        {
+            SortedList<T> newList;
+            Node* current = head;;
+            while (current != nullptr)
+            {
+                T temp = operation(current->value);
+                newList.insert(temp);
+                current = current->next;
+            }
+            return newList;
+        }
+
+        /**
+            * 12. apply - returns a new list with elements that were modified by an operation
+            */
     };
 
     template <class T>
-    class SortedList<T>::ConstIterator {
-    /**
-     * the class should support the following public interface:
-     * if needed, use =defualt / =delete
-     *
-     * constructors and destructor:
-     * 1. a ctor(or ctors) your implementation needs
-     * 2. copy constructor
-     * 3. operator= - assignment operator
-     * 4. ~ConstIterator() - destructor
-     *
-     * operators:
-     * 5. operator* - returns the element the iterator points to
-     * 6. operator++ - advances the iterator to the next element
-     * 7. operator!= - returns true if the iterator points to a different element
-     *
-     */
+    class SortedList<T>::ConstIterator
+    {
+        /**
+         * the class should support the following public interface:
+         * if needed, use =defualt / =delete
+         *
+         * constructors and destructor:
+         * 1. a ctor(or ctors) your implementation needs
+         * 2. copy constructor
+         * 3. operator= - assignment operator
+         * 4. ~ConstIterator() - destructor
+         *
+         * operators:
+         * 5. operator* - returns the element the iterator points to
+         * 6. operator++ - advances the iterator to the next element
+         * 7. operator!= - returns true if the iterator points to a different element
+         *
+         */
     };
 }
 
